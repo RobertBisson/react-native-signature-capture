28178c602b246dbba366eb0fba5c3a7dcdb009f9
Rob Bisson <rob.bisson@vmlearning.com.au>
2017-12-01 08:53:57 +1000

    Fix PropTypes in example.

--- Example/src/SignatureView.js
+++ Example/src/SignatureView.js
@@ -1,80 +1,79 @@
-import React, {
-  Component, PropTypes
-} from 'react';
+import React, { Component } from "react";
 
-import ReactNative, {
-  View, Text, Modal, Platform, Alert
-} from 'react-native';
-
-import SignatureCapture from 'react-native-signature-capture';
+import ReactNative, { View, Text, Modal, Platform, Alert } from "react-native";
+var PropTypes = require("prop-types");
+import SignatureCapture from "react-native-signature-capture";
 
 const toolbarHeight = Platform.select({
-  android: 0,
-  ios: 22
+    android: 0,
+    ios: 22
 });
 
 const modalViewStyle = {
-  paddingTop: toolbarHeight,
-  flex: 1
+    paddingTop: toolbarHeight,
+    flex: 1
 };
 
 class SignatureView extends Component {
-
-  static propTypes = {
-    onSave: PropTypes.func
-  }
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      visible: false
+    static propTypes = {
+        onSave: PropTypes.func
     };
-  }
 
-  show(display) {
-    this.setState({visible: display});
-  }
+    constructor(props) {
+        super(props);
 
-  render() {
-    const {visible} = this.state;
+        this.state = {
+            visible: false
+        };
+    }
 
-    return (
-      <Modal transparent={false} visible={visible} onRequestClose={this._onRequreClose.bind(this)}>
-        <View style={modalViewStyle}>
-          <View style={{padding: 10, flexDirection: 'row'}}>
-            <Text onPress={this._onPressClose.bind(this)}>{' x '}</Text>
-            <View style={{flex: 1, alignItems: 'center'}}>
-              <Text style={{fontSize: 14}}>Please write your signature.</Text>
-            </View>
-          </View>
-          <SignatureCapture
-            onDragEvent={this._onDragEvent.bind(this)}
-            onSaveEvent={this._onSaveEvent.bind(this)}
-          />
-        </View>
-      </Modal>
-    );
-  }
+    show(display) {
+        this.setState({ visible: display });
+    }
 
-  _onPressClose() {
-    this.show(false);
-  }
+    render() {
+        const { visible } = this.state;
 
-  _onRequreClose() {
-    this.show(false);
-  }
+        return (
+            <Modal
+                transparent={false}
+                visible={visible}
+                onRequestClose={this._onRequreClose.bind(this)}
+            >
+                <View style={modalViewStyle}>
+                    <View style={{ padding: 10, flexDirection: "row" }}>
+                        <Text onPress={this._onPressClose.bind(this)}>{" x "}</Text>
+                        <View style={{ flex: 1, alignItems: "center" }}>
+                            <Text style={{ fontSize: 14 }}>Please write your signature.</Text>
+                        </View>
+                    </View>
+                    <SignatureCapture
+                        onDragEvent={this._onDragEvent.bind(this)}
+                        onSaveEvent={this._onSaveEvent.bind(this)}
+                    />
+                </View>
+            </Modal>
+        );
+    }
 
-  _onDragEvent() {
-    // This callback will be called when the user enters signature
-   console.log("dragged");
-  }
+    _onPressClose() {
+        this.show(false);
+    }
 
-  _onSaveEvent(result) {
-    //result.encoded - for the base64 encoded png
-    //result.pathName - for the file path name
-    this.props.onSave && this.props.onSave(result);
-  }
+    _onRequreClose() {
+        this.show(false);
+    }
+
+    _onDragEvent() {
+        // This callback will be called when the user enters signature
+        console.log("dragged");
+    }
+
+    _onSaveEvent(result) {
+        //result.encoded - for the base64 encoded png
+        //result.pathName - for the file path name
+        this.props.onSave && this.props.onSave(result);
+    }
 }
 
 export default SignatureView;
